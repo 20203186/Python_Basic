9d8e1b7900ee51f4a0d2e8b60497ab8d8df271d5
20203186 <144189935+20203186@users.noreply.github.com>
2023-09-08 12:51:39 +0900

    Add python_01_variable

---
+++ lecture/python_01_variable.py
@@ -0,0 +1,10 @@
+# 주석(Comment) : 단순 메모, 개발 실행 X
+
+# 출력 함수(print)
+# - ()안의 값을 출력
+# - 변수 값 확인용도 또는 메시지 출력용도
+print("Hello Python2023")
+
+# 문자열 타입(String Type)
+# - Python: '', "" -> String
+# - C, Java; ''(Char), ""(String)
