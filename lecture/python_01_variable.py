1fba82bd2cfbacdf90ed0454c6122a693e6d28ba
20203186 <144189935+20203186@users.noreply.github.com>
2023-09-15 12:13:14 +0900

    Update Python_01_variable.py
    Finished

--- lecture/python_01_variable.py
+++ lecture/python_01_variable.py
@@ -6,7 +6,6 @@
 print("=" * 200)
 print("Hello Python2023")
 
-
 # 문자열 타입(String Type)
 # - Python: '', "" -> String
 # - C, Java; ''(Char), ""(String)
@@ -58,12 +57,12 @@ print("=" * 200)
 # Type Casting 실습
 int_a = 3
 float_b = 3.14
-str_c = "9.2"
+str_int_c = "9"
 str_float_d = "9.2"
 # 1) 문자열 정수형("9") -> 정수형 (9)
-print(int(str_int_c))
+#print(int(str_int_c))
 # 1-2) 문자열 정수형("9") -> 실수형 (9.0)
-print(float(str_float_c))
+#print(float(str_float_d))
 # 2) 문자열 실수형("9.2") -> 정수형 (Error)
 #print(int(str_int_d))
 # 2-2) 문자열 실수형("9.2") -> 실수형 9.2
@@ -108,3 +107,73 @@ print(num)  # 출력: 10
 # num = 5  # 문법
 # 대입연산자(=) : 우측의 값을 좌측에 저장
 # 동등연산자(==) : Equal
+
+# name 변수 생성
+# "SeongJin Cho" 값을 name 변수에 담기
+
+#name(변수명), =(대입연산자), "SeongJin Cho"(값)
+name = "SeongJin Cho"
+
+#명명규칙(Naming Rule)
+# * 변수, 함수, 클래스 등의 사용자 정의 이름을 붙일 때 사용!
+# * 명확하고 알아보기 쉽게 짓기!
+#
+#   1. 영문 대소문자, 숫자, 특수문자(_)만 사용
+#   2. 숫자로 시작할 수 없음
+#       abc1(o), 1abc(x)
+#   3. 영어 대소문자 구별
+#       abc, Abc ABc abC 모두 다른 변수!
+#   4. 예약어 사용 불가
+#      예약어: Python에서 미리 선점하여 사용중인 키워드
+#              ex) print, for, while, if, else~~~~~~~~
+#
+# Naming Method
+#   - 변수, 함수, 클래스 등의 사용자 정의 이름에 사용하는 기법
+#   - 프로그래밍 언어별로 사용하는 Naming Method가 다름!
+
+#   1.snake_case: 소문자만 사용, 합성어는 (_)사용
+#       ex) student_name
+#   2.camelCase: 첫글자 소문자, 합성어 첫글자 대문자
+#       ex) studentName
+#   3.PascalCase: 첫글자 대문자, 합성어 첫글자 대문자
+#       ex) StudentName
+
+#                 변수              함수          클래스
+#Java, C       camelCase        camelCase()     PascalCase
+#Python        snake_case       snake_Case()    PascalCase
+
+# 개발자: Web(프론트엔드, 백엔드), 앱(Andriod, Apple), 웹 퍼블리셔
+# 웹 디자이너
+# 서버 엔지니어(Linux 운영 관리)
+# 네트워크 엔지니어
+# 데이터베이스 엔지니어
+# SQL 튜너
+# 데이터 모델러
+# ERP 개발자
+# 보안 개발자
+# 인공지능 데이터 분석가 데이터 사이언티스트 프롬프트 엔지니어
+# 데이터 엔지니어
+
+# 동적 출력!
+print("="*200)
+student_num = 20203186
+student_name = "SeongJin Cho"
+
+#출력 예: "조선대학교 20203186, SeongJin Cho 입니다"
+print("조선대학교 20203186, SeongJin Cho 입니다.")  #하드코딩 지양!
+
+# 1. format() 함수 - Old
+print("조선대학교 {}, {} 입니다.".format(student_num, student_name))
+
+# 2. fstring - NeW
+print(f"조선대학교 {student_num}, {student_name} 입니다.")
+
+# 간단한 사칙연산
+#  + : 더하기
+#  - : 빼기
+#  * : 곱하기
+# ** or ^ : 제곱
+#  5/2 : 나누기 == 2.5
+#  5//2 : (나누기)몫 == 2
+#  5%2 : (나누기)나머지 == 1
+
