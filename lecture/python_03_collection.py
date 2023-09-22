12b26370fdb16e1b4df073c56a75d3e601f5afdc
20203186 <144189935+20203186@users.noreply.github.com>
2023-09-22 12:37:46 +0900

    update python_03_collection.py

--- lecture/python_03_collection.py
+++ lecture/python_03_collection.py
@@ -70,3 +70,119 @@ print(b)
 
 c = sorted(a, reverse=True)  # 내림차순
 print(c)
+
+# 2.튜플(Tuple) ex: 기차
+#  - List와 대부분 동일
+#  - 시퀀스 자료형(정렬 불가능)
+#  - immutable(생성된 후 변경 불가능)
+#  - index 사용(Slicing 가능)
+#  - packing과 Unpacking 가능
+#  - () 사용(생략 가능)
+#  * 직접 Tuple을 생성하는 경우 X
+#    -> 파이썬에서 결과값을 받을 때 Tuple로 제공
+
+print("="*100)
+a = [1, 2, 3]  #List
+b = (1, 2, 3)  #Tuple
+c = 1, 2, 3    #Tuple(괄호 생략 가능)
+
+a[0] = 99
+print(a)
+# b[0] = 99;
+# print(b)  # Tuple은 값 변경 불가능
+
+# 튜플 원소가 1개인 경우
+a = (1, 2, 3)  #Tuple
+b = 1, 2, 3  #Tuple
+c = (1)  #Tuple
+d = 1   #Int
+e = 1,  #Tuple
+print(type(b))
+print(type(d))
+print(type(e))
+
+a = 5
+b = 8
+
+# a, b 교환하는 코드 작성
+a, b = b, a  # Tuple 활용
+print(a)  # 8
+print(b)  # 5
+
+# 3.세트(Set) ex: 복주머니 or 가방
+#  - 수학의 집합 개념
+#  - 순서 없음(index 없음, 정렬 불가능)
+#  - 중복값을 허용하지 않음(중요)
+#  - {} 사용
+#  - 멤버함수: union(), intersection(), difference() 등
+set_a = {1, 2, 3}
+set_b = {1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5}
+print(set_b)
+
+# 중복값 제거 활용 방법
+#  : a List의 중복값을 제거
+a = ["A", "A", "B", "B", "C", "C", "D", "E"]  # list type
+# a = set(a)  # ()안의 값을 set type으로 변환
+# a = list(a)
+# List -> Set(중복값 제거) -> List
+a = list(set(a))
+print(a)
+print(type(a))
+
+# 4. 딕트 (dict) ex: 복주머니
+#  - 순서가 없음(인덱스 없음, 정렬 불가능)
+#  - {key : value} 사용 -> key, value 1 pair
+#  - key는 중복 불가, value 중복가능
+#  - key를 통해서만 value에 접근 가능
+#  - 멤버함수: update(), get(), keys(), values(), items()
+
+#  외부에서 데이터를 받아올 때 대부분 JSON 형식으로 전달
+#   - JSON == DICT(동일)
+
+# {"id" : "ccw1104", "pw" : "abc1234!", "name" : "ccW"}
+
+dict_a = {"korea": "Seoul",
+          "Canada": "Ottawa",
+          "USA": "Washington D.C"}
+print(dict_a)
+import pprint
+pprint.pprint(dict_a)
+
+# update() : dict와 dict 병합
+a = {"a" : 1,
+     "b" : 2}
+b = {"b" : 3,
+     "c" : 5}
+a.update(b)  # 병합시 중복key 있는 경우 입력값(b)이 우선
+print(a)
+
+# pop() : dict 원소를 삭제
+c = a.pop("a")
+print(a)
+print(c)  # {"a" : 1}삭제 된 value(key X)
+
+# in() : ()안의 key값이 존재 확인
+print("c" in a)
+print("f" in a)
+
+# get() : 값 접근
+# list, tuple, dict 접근 -> 컬렉션[index or key] ex : a[2]
+# print(a["f"])    # key가 없으면 error 발생
+print(a.get("f"))  # key가 없으면 None 출력 (error x)
+
+# keys(), values(), items()
+print(a.keys())         # key만 추출
+print(a.values())       # value만 추출
+print(a.items())        # (key, value) 추출
+
+print(list(a.keys()))   # 활용 방법
+
+# clear() : dict 초기화
+print(a)
+a.clear()
+print(a)
+
+# dict{}, set{} a = {} -> ??
+
+a = {}
+print(type(a))
