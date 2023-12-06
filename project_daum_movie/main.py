a846ca238931eb90b8b1ad0a4951d84cd373989a
20203186 <144189935+20203186@users.noreply.github.com>
2023-12-06 14:36:37 +0900

    complete project

--- project_daum_movie/main.py
+++ project_daum_movie/main.py
@@ -1,12 +1,20 @@
-from collect.collect_daum_movie_review import (review_collector)
+from collect.collect_daum_movie_review import review_collector
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 def main():
-    print("="*100)
+    print("=" * 100)
     print("== 영화 리뷰 수집기 ver1.0 ==")
-    print("="*100)
+    print("=" * 100)
     movie_code = input("== 영화코드: ")  # 169328
-    print('== MSG: "매일 낮 12시마다 수집됩니다."')
+    print('== MSG: "매일 낮 12시 수집"')
+    scheduler = BlockingScheduler()
+    scheduler.add_job(review_collector,   # Job
+                      trigger="cron",     # "Cron" 표기법 사용
+                      args=[movie_code],  # Job의 매개변수
+                      hours="12",         # 시간
+                      minute="0")         # 분
+    #scheduler.start()
     review_collector(movie_code)
 
 
